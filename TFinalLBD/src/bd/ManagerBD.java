fa5eeca8253e07a3275ab7f2fa9eb67c053ac7d4
Victor Krause <vkraause@hotmail.com>
2017-07-12 22:33:34 -0300

    Finalizado 5 e 7

--- TFinalLBD/src/bd/ManagerBD.java
+++ TFinalLBD/src/bd/ManagerBD.java
@@ -7,7 +7,6 @@ package bd;
 
 import java.sql.*;
 import java.util.ArrayList;
-import model.Funcionario;
 
 /**
  *
@@ -25,8 +24,7 @@ public class ManagerBD {
 	}
 
 	private Connection getConnection() throws SQLException {
-		Connection conexao = DriverManager.getConnection(
-				"jdbc:oracle:thin:@//camburi.pucrs.br:1521/facin11g",
+		Connection conexao = DriverManager.getConnection("jdbc:oracle:thin:@//camburi.pucrs.br:1521/facin11g",
 				"BG111019", "BG111019");
 
 		return conexao;
@@ -79,16 +77,54 @@ public class ManagerBD {
 		return strings;
 	}
 
-	public double getValorTotalDeReservaByEquipamentoId (int idEquipamento) throws SQLException{
+	//Se retornar -1 é porque não achou
+	public double getValorTotalDeReservaByEquipamentoId(int idEquipamento) throws SQLException {
 		double acum = 0;
 
+		Connection con = getConnection();
+		PreparedStatement stmt = con.prepareStatement("SELECT R.DATA_INICIAL, R.DATA_FINAL, E.CUSTO_DIARIA\n"
+				+ "FROM RESERVAS_TRAB R INNER JOIN EQUIPAMENTOS_TRAB E ON R.EQUIPAMENTO_ID = E.ID_EQUIPAMENTO\n"
+				+ "WHERE E.ID_EQUIPAMENTO = ?");
+		stmt.setInt(1, idEquipamento);
 
-		/**
-		for (dias = 0; dtFim.after(dyInicio); dias++) {  
-			dtFim.setDate(dtFim.getDate - 1);  
-		} 
-		 **/
+		ResultSet result = stmt.executeQuery();
 
+		if (result.next()) {
+			Date dtFim;
+			Date dtInicio;
+			int dias;
+			double custoDiario;
+
+			dtFim = result.getDate("DATA_FINAL");
+			dtInicio = result.getDate("DATA_INCIAL");
+			custoDiario = result.getDouble("CUSTO_DIARIA");
+
+			for (dias = 0; dtFim.after(dtInicio); dias++) {
+				dtFim.setDate(dtFim.getDate() - 1);
+			}
+			acum = custoDiario * dias;
+		}
+		else
+			acum = -1;
+		
+		result.close();
+		stmt.close();
+		con.close();
+		
 		return acum;
+
+	}
+
+	public ArrayList<String> getAllFuncionariosSemReserva() throws SQLException{
+		ArrayList<String> nomes = new ArrayList<String>();
+		
+		Connection con = getConnection();
+		Statement stmt = con.createStatement();
+		String sql = "SELECT F.NOME_COMPLETO\n"
+				+ "FROM FUNCIONARIOS_TRAB F\n"
+				+ "WHERE F.LOGIN NOT IN (SELECT E.FUNCIONARIO_LOGIN FROM EQUIPAMENTOS_TRAB E)\n"
+				+ "ORDER BY F.NOME_COMPLETO";
+		
+		return nomes;
 	}
 }
